672432b765ee4b52b766258bae0fd41082120f10
WJX2001 <1758807220@qq.com>
2024-11-02 12:04:50 +0800

    feat: 更改合约地址

--- src/constants/coins.js
+++ src/constants/coins.js
@@ -1,4 +1,4 @@
-import * as chains from './chains';
+import * as chains from './chains'
 
 const MAINNETCoins = [
   {
@@ -23,45 +23,47 @@ const MAINNETCoins = [
   {
     name: "Dai",
     abbr: "DAI",
-    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F", // not deployed
+    // address: "0x6B175474E89094C44Da98b954EedeAC495271d0F", 
+    address: '0x9e5AAC1Ba1a2e6aEd6b32689DFcF62A509Ca96f3'
   },
-  {
-    name: "Ampleforth",
-    abbr: "AMPL",
-    address: "0xD46bA6D942050d489DBd938a2C909A5d5039A161", // not deployed
-  },
+  // {
+  //   name: "Ampleforth",
+  //   abbr: "AMPL",
+  //   address: "0xD46bA6D942050d489DBd938a2C909A5d5039A161", // not deployed
+  // },
   {
     name: "Wrapped BTC",
     abbr: "WBTC",
-    address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", // not deployed
+    // address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", 
+    address: '0x00D84e62a854e54Ba7289ab6506F95000Bb4B008'
   },
-  {
-    name: "Fei USD",
-    abbr: "FEI",
-    address: "0x956F47F50A910163D8BF957Cf5846D573E7f87CA", // not deployed
-  },
-  {
-    name: "frax",
-    abbr: "FRAX",
-    address: "0x853d955aCEf822Db058eb8505911ED77F175b99e", // not deployed
-  },
-  {
-    name: "Frax Share",
-    abbr: "FXS",
-    address: "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0", // not deployed
-  },
-  {
-    name: "renBTC",
-    abbr: "renBTC",
-    address: "0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D", // not deployed
-  },
-  {
-    name: "StakeWise",
-    abbr: "SWISE",
-    address: "0x48C3399719B582dD63eB5AADf12A40B4C3f52FA2", // not deployed
-  },
+  // {
+  //   name: "Fei USD",
+  //   abbr: "FEI",
+  //   address: "0x956F47F50A910163D8BF957Cf5846D573E7f87CA", // not deployed
+  // },
+  // {
+  //   name: "frax",
+  //   abbr: "FRAX",
+  //   address: "0x853d955aCEf822Db058eb8505911ED77F175b99e", // not deployed
+  // },
+  // {
+  //   name: "Frax Share",
+  //   abbr: "FXS",
+  //   address: "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0", // not deployed
+  // },
+  // {
+  //   name: "renBTC",
+  //   abbr: "renBTC",
+  //   address: "0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D", // not deployed
+  // },
+  // {
+  //   name: "StakeWise",
+  //   abbr: "SWISE",
+  //   address: "0x48C3399719B582dD63eB5AADf12A40B4C3f52FA2", // not deployed
+  // },
 ]
 
-const COINS = new Map();
-COINS.set(chains.ChainId.MAINNET, MAINNETCoins);
+const COINS = new Map()
+COINS.set(chains.ChainId.MAINNET, MAINNETCoins)
 export default COINS
